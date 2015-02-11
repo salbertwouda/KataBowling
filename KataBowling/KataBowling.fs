1993dde6aeaed57c16bcaa80de0d1da25002b6e4
Salbert Wouda <salbertwouda@gmail.com>
2015-02-11 21:34:32 +0100

    KataBowling implementation

---
+++ KataBowling/KataBowling.fs
@@ -0,0 +1,66 @@
+﻿module KataBowling
+
+type FinalTurnResult =
+    | ThreeStrikes
+    | TwoStrikes of int
+    | Spare of int
+    | Points of int*int
+
+type TurnResult = 
+    | Strike
+    | Spare of int
+    | Points of int*int
+    | Final of FinalTurnResult
+
+let rec private getPointsOfAllTries = function
+    | Strike -> 10
+    | Spare _ -> 10
+    | Points (x,y) -> x+y
+    | Final r -> 
+        match r with
+        | ThreeStrikes -> 30
+        | TwoStrikes p -> 20 + p
+        | FinalTurnResult.Spare p -> 10 + p
+        | FinalTurnResult.Points (x,y) -> x + y
+
+let rec private getPointsOfFirstTry = function
+    | Strike -> 10
+    | Spare p -> p
+    | Points (p,_) -> p
+    | Final r -> 
+        match r with
+        | ThreeStrikes
+        | TwoStrikes _ -> 10
+        | FinalTurnResult.Spare p -> p
+        | FinalTurnResult.Points (x,_) -> x
+
+let rec private collectBonuses results = 
+    match results with
+        | Strike::first::second::tail -> 
+            let secondPoints = second |> getPointsOfAllTries
+            let rest = [first;second] @ tail |> collectBonuses 
+            
+            first
+            |> getPointsOfAllTries
+            |> (+) secondPoints
+            |> (+) rest
+        | (Spare _)::first::tail -> 
+            let rest = [first]@tail |> collectBonuses
+            
+            first
+            |> getPointsOfFirstTry
+            |> (+) rest
+        | _::tail -> 
+            (tail |> collectBonuses)
+        | [] -> 0
+
+let rec private collectScores results = 
+    results
+    |> List.map getPointsOfAllTries
+    |> List.sum
+
+let rec calculateGameScore results = 
+    let bonus = results |> collectBonuses
+    results 
+    |> collectScores
+    |> (+) bonus
---
+++ KataBowling/Tests.fs
@@ -0,0 +1,26 @@
+﻿module Tests
+open NUnit.Framework
+open FsUnit
+open KataBowling
+
+let createGameResults resultOfNormalTurn resultOfFinalTurn= 
+    ([1..9] |> List.map (fun _ -> resultOfNormalTurn))
+    @ [resultOfFinalTurn]
+    
+[<Test>]
+let ``When the score of a perfect game is calculated the result should equal 300`` () =
+    createGameResults Strike (Final ThreeStrikes) 
+    |> calculateGameScore
+    |> should equal 300    
+
+[<Test>]
+let ``When the score of a game with only Spare 5 is calculated the result should equal 150`` () =
+    createGameResults (Spare 5) (Final (FinalTurnResult.Spare 5))
+    |> calculateGameScore
+    |> should equal 150
+
+[<Test>]
+let ``When the score of a game with only Points 9,0 is calculated the result should equal 90`` () =
+    createGameResults (Points (9,0)) (Final (FinalTurnResult.Points (9,0)))
+    |> calculateGameScore
+    |> should equal 90    
